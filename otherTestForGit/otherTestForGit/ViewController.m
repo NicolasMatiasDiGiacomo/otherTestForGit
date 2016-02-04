0d91a3660601cc856941d61c0ae80172a4d493ef
Nicolas Matias Di Giacomo <nicolasmatiasdigiacomo@Nicolass-Mac-mini.local>
2016-02-04 14:54:17 -0300

    initial commit

---
+++ otherTestForGit/otherTestForGit/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  otherTestForGit
+//
+//  Created by Nicolas Matias Di Giacomo  on 2/4/16.
+//  Copyright © 2016 Nicolas Matias Di Giacomo . All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //mofoasmgo
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ otherTestForGit/otherTestForGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  otherTestForGit
+//
+//  Created by Nicolas Matias Di Giacomo  on 2/4/16.
+//  Copyright © 2016 Nicolas Matias Di Giacomo . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
